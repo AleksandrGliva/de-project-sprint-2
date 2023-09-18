9bec022b5cec781c33c94f8458cc66b8f1d114da
AleksandrGliva <83962743+AleksandrGliva@users.noreply.github.com>
2023-09-18 20:37:40 +0400

    Update create_tables_script.sql

--- create_tables_script.sql
+++ create_tables_script.sql
@@ -1,19 +1,14 @@
 DROP 
   TABLE IF EXISTS public.shipping_country_rates;
-DROP 
-  TABLE IF EXISTS public.shipping_agreement;
-DROP 
-  TABLE IF EXISTS public.shipping_transfer;
-DROP 
-  TABLE IF EXISTS public.shipping_info;
-DROP 
-  TABLE IF EXISTS public.shipping_status;
 CREATE TABLE public.shipping_country_rates (
   id SERIAL, 
   shipping_country VARCHAR(56), 
   shipping_country_base_rate NUMERIC(14, 2), 
   CONSTRAINT scr_id_pkey PRIMARY KEY (id)
 );
+
+DROP 
+  TABLE IF EXISTS public.shipping_agreement;
 CREATE TABLE public.shipping_agreement (
   agreement_id SMALLINT, 
   agreement_number VARCHAR(12), 
@@ -21,6 +16,9 @@ CREATE TABLE public.shipping_agreement (
   agreement_commission NUMERIC(14, 2), 
   CONSTRAINT sa_agreement_id_pkey PRIMARY KEY (agreement_id)
 );
+
+DROP 
+  TABLE IF EXISTS public.shipping_transfer;
 CREATE TABLE public.shipping_transfer (
   id SERIAL, 
   transfer_type VARCHAR(2), 
@@ -28,6 +26,9 @@ CREATE TABLE public.shipping_transfer (
   shipping_transfer_rate NUMERIC(14, 3), 
   CONSTRAINT st_id_pkey PRIMARY KEY (id)
 );
+
+DROP 
+  TABLE IF EXISTS public.shipping_info;
 CREATE TABLE public.shipping_info (
   shipping_id INT, 
   vendor_id SMALLINT, 
@@ -41,6 +42,9 @@ CREATE TABLE public.shipping_info (
   CONSTRAINT si_shipping_agreement_id_fkey FOREIGN KEY (shipping_agreement_id) REFERENCES shipping_agreement (agreement_id) ON UPDATE cascade, 
   CONSTRAINT si_shipping_country_rates_id_fkey FOREIGN KEY (shipping_country_rates_id) REFERENCES shipping_country_rates (id) ON UPDATE cascade
 );
+
+DROP 
+  TABLE IF EXISTS public.shipping_status;
 CREATE TABLE public.shipping_status (
   shipping_id INT, 
   status VARCHAR(15), 
